0e359b57a9deb7177083f1688c71d0a8690deaae
jose <jagonzalez.alumno@iesrafaelalberti.es>
2022-10-31 12:50:45 +0100

    papelera para eliminar

--- codigophp/listaUsuario.php
+++ codigophp/listaUsuario.php
@@ -3,16 +3,41 @@
 require("testlogin.php");
 require("conecta.php");
 
-$nombre = $_SESSION["usuario"];
-$id_usuario = $_SESSION["id_usuario"];
+?>
 
-$memesSQL ="SELECT * FROM meme WHERE id_usuario = :id_usuario";
-$datosSql=array("id_usuario"=>$_SESSION['id_usuario']);
-$stmt= $conn->prepare($memesSQL);
-$stmt->execute($datosSql);
-$memes = $stmt->fetchAll();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+</head>
+<body>
+<header>
+</header>
 
-foreach($memes as $meme){
-    echo("<img width='200px' src='memes/".$meme['ruta']."?id_meme=".$meme['id_meme']."'>");
+<main>
+    <?php
 
-}
+    $nombre = $_SESSION["usuario"];
+    $id_usuario = $_SESSION["id_usuario"];
+
+    $memesSQL ="SELECT * FROM meme WHERE id_usuario = :id_usuario";
+    $datosSql=array("id_usuario"=>$_SESSION['id_usuario']);
+    $stmt= $conn->prepare($memesSQL);
+    $stmt->execute($datosSql);
+    $memes = $stmt->fetchAll();
+
+    foreach($memes as $meme){
+        echo("<img width='200px' src='memes/".$meme['ruta']."?id_meme=".$meme['id_meme']."'>");
+        echo("<a href='borrarmeme.php?id_meme=".$meme["id_meme"]."' ><i class='fa-solid fa-trash'></i></a>") ;
+
+    }
+
+   ?>
+</main>
+
+</body>
+</html>
